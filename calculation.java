b6859b1140754e57dd7384f55acd998a5c9de6b3
PKRUNAL867 <prajapatipinki1112@gmail.com>
2024-05-30 08:21:24 +0530

    Add files via upload

---
+++ calculation.java
@@ -0,0 +1,22 @@
+package staticclass;
+
+public class calculation {
+	static int count=0;
+	int ct=0;
+	
+	
+	public calculation()
+	{
+		count++;
+		ct++;
+	}
+
+
+	@Override
+	public String toString() {
+		return "calculation ct=" + ct+"   " + "count"+count+" ";
+	}
+
+	
+
+}
---
+++ democal.java
@@ -0,0 +1,18 @@
+package staticclass;
+
+public class democal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		calculation c1=new calculation();
+		System.out.print(c1);
+		calculation c3=new calculation();
+		System.out.print(c3);
+		calculation c2=new calculation();
+		System.out.print(c2);
+		
+		System.out.print(calculation.count);
+
+	}
+
+}
